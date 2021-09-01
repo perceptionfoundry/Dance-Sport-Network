93734413e97fd8ed9aee7bce985fe75fa8032502
Shahrukh Haider <31928839+perceptionfoundry@users.noreply.github.com>
2021-09-01 13:44:29 +0500

    bottomcard view

---
+++ Dance Sport Network/Dance Sport Network/View/BottomCardView.swift
@@ -0,0 +1,68 @@
+//
+//  BottomCardView.swift
+//  EDYOU App
+//
+//  Created by Syed ShahRukh Haider on 16/08/2021.
+//
+
+import SwiftUI
+
+
+struct BottomCardView<Content:View>: View {
+    var content : Content
+    @Binding var cardShown: Bool
+    @Binding var cardDismissal : Bool
+    let height : CGFloat
+    
+    init(shown: Binding<Bool>,
+         Dismissal: Binding<Bool>,
+         height : CGFloat,
+        @ViewBuilder content: ()-> Content){
+        self.content = content()
+        _cardShown = shown
+        _cardDismissal = Dismissal
+        self.height = height
+    }
+    var body: some View {
+       
+        ZStack{
+        // DIMMED
+        
+            GeometryReader{ _ in
+                EmptyView()
+            }
+            .background(Color.black.opacity(0.41))
+            .opacity(cardShown ? 1 : 0)
+            .animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
+            .onTapGesture {
+                //DISMISSAL
+                cardDismissal.toggle()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+                    cardShown.toggle()
+                }
+            }
+        // CARD
+            
+            VStack{
+                
+                Spacer()
+                VStack{
+                    Capsule()
+                        .fill(Color.gray)
+                        .frame(width: 80, height: 4, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .padding(.top, 4)
+                    content
+                        
+                }
+                .background(Color.white)
+                .cornerRadius(50)
+                .frame(height:height)
+                .offset(y : cardDismissal && cardShown ? 0 : height)
+                .animation(.default.delay(0.5))
+            }
+            
+        }.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+       
+    }
+}
+
